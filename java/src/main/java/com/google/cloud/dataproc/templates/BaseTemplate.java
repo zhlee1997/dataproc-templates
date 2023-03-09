a291b04ded23b4bf222efed2be21519e01d12e9b
saumyasinha-google <saumyasinha@google.com>
2023-03-09 17:00:30 +0530

    feat: Added template Name

--- java/src/main/java/com/google/cloud/dataproc/templates/BaseTemplate.java
+++ java/src/main/java/com/google/cloud/dataproc/templates/BaseTemplate.java
@@ -52,7 +52,8 @@ public interface BaseTemplate {
     SNOWFLAKETOGCS,
     JDBCTOSPANNER,
     PUBSUBTOBIGTABLE,
-    GCSTOBIGTABLE
+    GCSTOBIGTABLE,
+    PUBSUBLITETOGCS
   }
 
   default Properties getProperties() {
